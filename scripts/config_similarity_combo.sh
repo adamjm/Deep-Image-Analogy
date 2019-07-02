e253c477b6ca43df6e698eb45e847bc93da02941
Adam Makarucha <adamm@opfscl06.syd.iic.ihost.com>
2019-07-03 00:10:04 +1000

    Changed model download

--- scripts/config_similarity_combo.sh
+++ scripts/config_similarity_combo.sh
@@ -4,4 +4,4 @@ protoc caffe/proto/caffe.proto --cpp_out=../include/
 
 cd ..
 
-wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel -P similarity_combo/models/vgg19
+wget -c https://www.dropbox.com/s/mnsxsfv5non3e81/vgg19_bn_gray_ft_iter_150000.caffemodel?dl=0 -P similarity_combo/models/vgg19_bn_gray_ft_iter_150000.caffemodel
